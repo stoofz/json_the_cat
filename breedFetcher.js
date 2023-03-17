48cad0c7b0188ed21c48719c0d32cec0c489b640
Anthony Chambers <ajfchambers@gmail.com>
2023-03-17 12:59:15 -0400

    refactor for index.js calls

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,23 @@
 const request = require('request');
 
-const api = 'https://api.thecatapi.com/v1/breeds/search?name=';
-const search = process.argv.slice(2);
+const fetchBreedDescription = function(breed, cb) {
+  
+  const api = `https://api.thecatapi.com/v1/breeds/search?name=${breed}`;
 
-const getCat = request(api + search, function(err, response, body) {
+  request(api, function(err, response, body) {
+    console.log(null);
+    if (err) {
+      return cb(null, err);
+    }
 
-  if (err) {
-    console.log(err);
-  }
+    const data = JSON.parse(body);
 
-  const data = JSON.parse(body);
+    if (data[0] !== undefined && breed.toLowerCase() === data[0]['name'].toLowerCase()) {
+      return cb(null, (null, data[0]['description']));
+    }
 
-  if (data[0] !== undefined && search[0].toLowerCase === data[0]['name'].toLowerCase) {
-    return console.log(data[0]['description']);
-  }
+    return cb(`${breed} breed not found`);
+  });
+};
 
-  return console.log(`${search[0]} breed not found`);
-});
-
-getCat;
+module.exports = { fetchBreedDescription };
